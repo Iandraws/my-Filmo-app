e637a6fb48a5b612dfba8345c64e148ec292027c
Iandraws <abrahamxman2@gmail.com>
2023-04-30 22:47:37 +0200

    make the http requset and fetch data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MovieDetailsService } from './movie-details.service';
+import { OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-Filmo-app';
+export class AppComponent implements OnInit {
+  
+
+    constructor(private movieDetailsService: MovieDetailsService) { }
+  
+    ngOnInit(): void {
+      this.movieDetailsService.getMovieDetails().subscribe((data) => {
+        console.log(data);
+      });
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
+
+  
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
+    ,MatToolbarModule
+    ,MatIconModule
+    ,MatButtonModule
+    ,MatInputModule
+    ,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
