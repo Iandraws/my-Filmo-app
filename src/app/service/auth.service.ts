9455728140ab9fe6561ce1614f0d9746fe23978a
Ibrahim Andraws <abrahamxman2@gmail.com>
2023-06-02 09:15:36 +0200

    make the register and fix somethings

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,21 +3,26 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private apiUrl = 'https://demo.parklolo.com/api';
   private localStorageKey = 'accessToken';
   isLoggedIn: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  login(credentials: { identifier: string, password: string }): Observable<Object> {
+  login(credentials: {
+    identifier: string;
+    password: string;
+  }): Observable<Object> {
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     });
 
-    return this.http.post<Object>(`${this.apiUrl}/login`, credentials, { headers });
+    return this.http.post<Object>(`${this.apiUrl}/login`, credentials, {
+      headers,
+    });
   }
 
   saveAccessToken(token: string): void {
@@ -27,9 +32,27 @@ export class AuthService {
   getAccessToken(): string | null {
     return localStorage.getItem(this.localStorageKey);
   }
-  
 
   clearAccessToken(): void {
     localStorage.removeItem(this.localStorageKey);
+  }
+
+  register(credentials: {
+    displayName: string;
+    username: string;
+    email: string;
+    password: string;
+    isAdmin: boolean;
+    active: boolean;
+  }): Observable<Object> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      
+
+    });
+
+    return this.http.post<Object>(`${this.apiUrl}/employees`, credentials, {
+      headers,
+    });
   }
 }
